0f67be8d706975a6f9b260b1877887080e990aa1
hamza kilic <hamza@hamzakilic.com>
2017-03-25 23:18:07 +0300

    layout is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { DropdownModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { StatusbarComponent } from './layout/statusbar/statusbar.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { MenubarComponent } from './layout/menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+
+    LayoutComponent,
+
+    StatusbarComponent,
+
+    TopbarComponent,
+
+    MenubarComponent
   ],
   imports: [
+    DropdownModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/layout/statusbar/statusbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'layout-statusbar',
+  templateUrl: './statusbar.component.html',
+  styleUrls: ['./statusbar.component.scss']
+})
+export class StatusbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
